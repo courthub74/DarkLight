2f86e732cc4e280cdf2bcff8df80a2ae659cbe9b
Courdevelops <courdevelops@gmail.com>
2024-09-01 14:19:11 -0400

    no logos for now

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ let day_logo = document.getElementById('day');
 // add event listener to button
 switch_button.addEventListener('click', () => {
     console.log("Switch Background");
-    // Toggle the Sun on
-    day_logo.classList.toggle('appear');
-    // Toggle the Moon off
-    nite_logo.classList.toggle('dissapear');
+    // // Toggle the Sun on
+    // day_logo.classList.toggle('appear');
+    // // Toggle the Moon off
+    // nite_logo.classList.toggle('dissapear');
     // Get the Root Element
         // its already defined in the html element
     const rootElem = document.documentElement
@@ -31,4 +31,15 @@ switch_button.addEventListener('click', () => {
 
     // set localStorage for the screen mode to stay put
     localStorage.setItem('theme', newTheme);
+
+    // If screenmode is light then set button to the moon
+    // create a variable and set it to the conditional like above
+    // buttonLogo = (screen_mode === 'light') ? 'dark' : 'light'
+
+    // Set the HTML attribute to the button logo (appends the two)
+    // nite_logo = document.documentElement
+    // console.log(nite_logo);
+    // set the class of the logo based on the html attribute
+    // nite_logo.setAttribute('screen_mode', buttonLogo);
+    // conditional for logos
 });
