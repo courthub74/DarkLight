c38b451dfe37044fbc5fe422b048831f4192d600
Courdevelops <courdevelops@gmail.com>
2024-08-31 20:14:33 -0400

    button works

--- app.js
+++ app.js
@@ -6,9 +6,17 @@
 // Query the Switch button
 let switch_button = document.getElementById('switch_button');
 
+// Query the Moon and Sun
+let nite_logo = document.getElementById('nite');
+let day_logo = document.getElementById('day'); 
+
 // add event listener to button
 switch_button.addEventListener('click', () => {
     console.log("Switch Background");
+    // Toggle the Sun on
+    day_logo.classList.toggle('appear');
+    // Toggle the Moon off
+    nite_logo.classList.toggle('dissapear');
     // Get the Root Element
         // its already defined in the html element
     const rootElem = document.documentElement
